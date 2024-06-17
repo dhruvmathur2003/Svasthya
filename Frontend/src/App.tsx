75e8719876b73c48fd135d724b732f51437b011c
Dhruv <dhruvmathur2003@gmail.com>
2024-06-17 11:02:38 +0530

    FooterAdded

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/LandingPage'
+import SignUp from './pages/SignUp';
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<LandingPage/>}/>
-      
+      <Route path="signup" element={<SignUp/>}/>
     </Routes>
 </Router>
   )
---
+++ Frontend/src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+const SignUp = ()=>{
+return (
+    <>
+    <h1>
+        Hello Boi SignUp Plzzzzzzz!!!
+    </h1>
+    </>
+)
+}
+
+export default SignUp;
